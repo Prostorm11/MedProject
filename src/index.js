e847d110ea2ca4055b6a777e3ee8b958d1c1d1dc
Fiifi Gorman <gormanshaphat@gmail.com>
2025-03-12 19:54:32 +0000

    adjusted the title

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 /* import SignupScreen from './SignUp/signup'; */
-/* import LandingPage from './LandingPage/landing'; */
-import Diabetes from './Disease checker/diabetes';
+import LandingPage from './LandingPage/landing';
+// import Diabetes from './Disease checker/diabetes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <LandingPage></LandingPage> */}
+    <LandingPage></LandingPage>
     {/* <SignupScreen></SignupScreen> */}
-    <Diabetes></Diabetes>
+    {/* <Diabetes></Diabetes> */}
    
   </React.StrictMode>
 );
