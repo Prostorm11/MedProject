55bc82e8830401cde4523184fc49d9b307d55e8f
Derrick Marfo <steammarfo111@gmail.com>
2025-03-15 13:21:30 +0000

    autismprogress

--- src/Reusables/Sqauredquestions.jsx
+++ src/Reusables/Sqauredquestions.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 import "./Squaredquestions.css";
+import SquaredPressable from "./squaredPressable";
+import Pressable from "./pressable";
 
 function Sqauredquestions({ question, options, action }) {
   return (
     <div id="mainbox">
       <div id="responsecolumn">
-        {options.map((item,index) => (
+        {options.map((item, index) => (
           <p>{item}</p>
         ))}
       </div>
       <div id="questioncolumn">
-        <div>
+        <div id="ask">
           <p>{question}</p>
         </div>
-        <button onClick={action}>Next</button>
+        <div className="questionChange">
+          <Pressable padd={"15px"} height={"10px"} width={"70px"} radius={"10px"}>Prev</Pressable>
+          <Pressable padd={"15px"} height={"10px"} width={"70px"} radius={"10px"}>Next</Pressable>
+          
+        </div>
       </div>
     </div>
   );
