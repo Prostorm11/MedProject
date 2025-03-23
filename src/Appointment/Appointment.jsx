7a6bcf41b567a485e0358631bfbc841d3cf8ca21
Derrick Marfo <steammarfo111@gmail.com>
2025-03-23 11:45:18 +0000

    progress(Appointment)

--- src/Appointment/Appointment.jsx
+++ src/Appointment/Appointment.jsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useState } from "react";
+import "./Appointment.css";
+import { motion } from "framer-motion";
+import DoctorInfo from "../Reusables/DoctorInfo";
+import { Doctors } from "./Doctors";
 
 function Appointment() {
-    return (
-        <div>
-            <p>Hello Appointment</p>
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  function handleContact() {
+    setIsModalOpen(true);
+  }
+  return (
+    <div className="appointmentBoard">
+      {Doctors.map((item, index) => (
+        <motion.div
+          whileHover={{ scale: 1.1 }}
+          transition={{ duration: 0.3 }}
+          style={{
+            width: 400,
+            height: 300,
+            overflow: "hidden",
+            borderRadius: "15px",
+            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
+          }}
+        >
+          <DoctorInfo
+            source={item.image}
+            name={item.details.name}
+            age={item.details.age}
+            hospital={item.details.Hostipital}
+            speciality={item.details.Specialist}
+            oncontact={handleContact}
+          />
+        </motion.div>
+      ))}
+      {isModalOpen && (
+        <div className="modal-overlay">
+          <div className="modal">
+          <span className="close" onClick={() => setIsModalOpen(false)}>
+            &times;
+          </span>
+          </div>
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
 export default Appointment;
